7ec687a40508e6782c4f5abc2452df3ca56d4c6b
ImmanuelTolstoyevski <mert.yurekli02@icloud.com>
2018-10-30 19:54:19 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -23,21 +23,21 @@ class App extends React.Component {
   const data = await api_call.json();
  if (city && country) { console.log(data);
   this.setState({
-    temperature: data.main.temp,
+    temperature: data.temp,
     city: data.name,
     country: data.sys.country,
     humidity: data.main.humidity,
     description: data.weather[0].description,
     error: ""
   });
-} else {
+} else  {
   this.setState({
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: "Please enter the value."
+    temperature : undefined,
+    city : undefined,
+    country : undefined,
+    humidity : undefined,
+    description : undefined,
+    error : "Please enter the value."
   });
 }
   }
