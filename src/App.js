805f0b1982a2635c35d9f9b8df00a6537cb5b031
ImmanuelTolstoyevski <mert.yurekli02@icloud.com>
2018-11-14 20:17:28 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
   const data = await api_call.json();
  if (city && country) { console.log(data);
   this.setState({
-    temperature: data.main.list[0].temp,
+    temperature: data.main[temp],
     city: data.name,
     country: data.sys.country,
     humidity: data.main.humidity,
